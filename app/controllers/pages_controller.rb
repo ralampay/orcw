7c3f30abab768619ed78fac26d793e7d2739fe2d
Raphael Alampay <raphael.alampay@gmail.com>
2019-02-26 07:26:42 +0800

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def rcw_faculty
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  get "/about", to: "pages#about"
+  get "/rcw_faculty", to: "pages#rcw_faculty"
+  # This line mounts Refinery's routes at the root of your application.
+  # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
+  # If you would like to change where this extension is mounted, simply change the
+  # configuration option `mounted_path` to something different in config/initializers/refinery/core.rb
+  #
+  # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
+  mount Refinery::Core::Engine, at: Refinery::Core.mounted_path
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
